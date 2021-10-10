ea8a7c3bcb866f8f158d9f8af920325b88e24e97
rachid-oubaha <91636761+rachid-oubaha@users.noreply.github.com>
2021-10-11 01:53:11 +0200

    Create main.py

---
+++ Stack/main.py
@@ -0,0 +1,17 @@
+from Stack import Stack
+
+
+stack = Stack()
+
+stack.push("a")
+stack.push("b")
+stack.push("c")
+stack.push("d")
+
+print(stack.stack)
+
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
